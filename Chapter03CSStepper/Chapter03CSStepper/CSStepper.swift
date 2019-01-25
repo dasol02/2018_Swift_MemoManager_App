9e7d28ab75a6cb7256a08f21c85b08b823e1aeac
leeds1204 <leeds1204@gaeasoft.co.kr>
2019-01-25 14:34:21 +0900

    [ Chapter03 ] 기능 추가 - 갑 변경시 메서드 호출 이벤트 (로깅 지원등) - 스토리보드에 설정 가능하게 기능 지원 @IBInspectable

--- Chapter03CSStepper/Chapter03CSStepper/CSStepper.swift
+++ Chapter03CSStepper/Chapter03CSStepper/CSStepper.swift
@@ -3,13 +3,59 @@ import UIKit
 // 스토리보드에서 미리보기로 출력 가능하게 지원
 @IBDesignable
 
-class CSStepper: UIView {
+class CSStepper: UIControl {
     
     // 버튼 및 라벨 선언
     public var leftBtn = UIButton(type: UIButton.ButtonType.system)
     public var rightBtn = UIButton(type: UIButton.ButtonType.system)
     public var centerLabel = UILabel()
-    public var value: Int = 0 //초기값 지정
+    
+    // 프로퍼티 옵저버 설정
+    // 왼쪽 버튼 텍스트
+    @IBInspectable // 스토리 보드에서 설정 가능하게 노출 지원
+    public var leftBtnTitle: String = "⬇︎"{
+        didSet{
+            self.leftBtn.setTitle(leftBtnTitle, for: UIControl.State.normal)
+        }
+    }
+    
+    // 왼쪽 버튼 텍스트
+    @IBInspectable
+    public var rightBtnTitle: String = "⬆︎"{
+        didSet{
+            self.rightBtn.setTitle(rightBtnTitle, for: UIControl.State.normal)
+        }
+    }
+    
+    // 레이블 영역 색상
+    @IBInspectable
+    public var bgColor: UIColor = UIColor.cyan {
+        didSet{
+            self.centerLabel.backgroundColor = bgColor
+        }
+    }
+    
+    // 노출 값
+    @IBInspectable
+    public var value: Int = 0 { //초기값 지정
+        didSet{
+            self.centerLabel.text = String(value)
+            
+            // 현재값이 바뀔 경우 valueChanged이벤트 호출
+            self.sendActions(for: UIControl.Event.valueChanged)
+        }
+    }
+    
+    // 버튼 증감값 단위
+    @IBInspectable
+    public var stepValue: Int = 1
+    
+    // 최댓값과 최솟값
+    @IBInspectable public var maximumValue: Int = 100
+    @IBInspectable public var minimumValue: Int = -100
+    
+    
+// MARK: init
     
     // 스토리 보드 호출시
     public required init?(coder aDecoder: NSCoder) {
@@ -32,14 +78,14 @@ class CSStepper: UIView {
         
         // 좌측 버튼 설정
         self.leftBtn.tag = -1
-        self.leftBtn.setTitle("⬇︎", for: UIControl.State.normal)
+        self.leftBtn.setTitle(leftBtnTitle, for: UIControl.State.normal)
         self.leftBtn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
         self.leftBtn.layer.borderWidth = borderWidth
         self.leftBtn.layer.borderColor = borderColor
         
         // 우측 버튼 설정
         self.rightBtn.tag = 1
-        self.rightBtn.setTitle("⬆︎", for: UIControl.State.normal)
+        self.rightBtn.setTitle(rightBtnTitle, for: UIControl.State.normal)
         self.rightBtn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
         self.rightBtn.layer.borderWidth = borderWidth
         self.rightBtn.layer.borderColor = borderColor
@@ -48,16 +94,21 @@ class CSStepper: UIView {
         self.centerLabel.text = String(value) // 현재 값을 문자열로 변환하여 표시
         self.centerLabel.font = UIFont.systemFont(ofSize: 16)
         self.centerLabel.textAlignment = .center
-        self.centerLabel.backgroundColor = UIColor.cyan
+        self.centerLabel.backgroundColor = self.bgColor
         self.centerLabel.layer.borderWidth = borderWidth
         self.centerLabel.layer.borderColor = borderColor
         
+        // 이벤트 연결
+        self.leftBtn.addTarget(self, action: #selector(valueChange(_:)), for: UIControl.Event.touchUpInside)
+        self.rightBtn.addTarget(self, action: #selector(valueChange(_:)), for: UIControl.Event.touchUpInside)
         
         // 화면에 추가
         self.addSubview(leftBtn)
         self.addSubview(rightBtn)
         self.addSubview(centerLabel)
     }
+    
+    // MARK: 변수 선언
     
     // 뷰 크기에 따라 버튼과 레이블 크기 설정
     override public func layoutSubviews() {
@@ -73,6 +124,29 @@ class CSStepper: UIView {
         self.rightBtn.frame = CGRect(x: btnWidth+labelWidth, y: 0, width: btnWidth, height: btnWidth)
         
     }
+    
+    // MARK: Action
+    // 버튼 이벤트에 따라 레이블 값 변경 메서드
+    @objc public func valueChange(_ sender: UIButton){
+        
+        // 값 변경시 최댓, 최솟값 범위 유효 체크
+        let sum = self.value + (sender.tag * self.stepValue)
+        
+        // 최댓값 보다 클 경우
+        if sum > self.maximumValue {
+            return
+        }
+        
+        // 최솟값 보다 작을 경우
+        if sum < self.minimumValue {
+           return
+        }
+        
+        // 현재 벨류값 수정
+        self.value = sum
+        
+    }
+    
     
     /*
     // Only override draw() if you perform custom drawing.
--- Chapter03CSStepper/Chapter03CSStepper/ViewController.swift
+++ Chapter03CSStepper/Chapter03CSStepper/ViewController.swift
@@ -2,16 +2,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var storyBoardBtn: CSStepper!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        // 코드로 버튼 생성
         let stepper = CSStepper()
-        
         stepper.frame = CGRect(x: 30, y: 100, width: 130, height: 30)
+        stepper.addTarget(self, action: #selector(logging(_:)), for: UIControl.Event.valueChanged)
+        stepper.tag = 0
+        
+        // 스토리 보드 생성 버튼 이벤트 연결
+        self.storyBoardBtn.addTarget(self, action: #selector(logging(_:)), for: UIControl.Event.valueChanged)
+        self.storyBoardBtn.tag = 1
+        
+        
         self.view.addSubview(stepper)
     }
 
+    // 스테퍼 메서드 (값 변경시)
+    @objc func logging(_ sender: CSStepper){
+        if sender.tag == 0 {
+            NSLog("\n코드로 생성된 스테퍼의 현재 값은 \(sender.value)")
+        }else if sender.tag == 1 {
+            NSLog("\n스토리보드로 생성된 스테퍼의 현재 값은 \(sender.value)")
+        }
+        
+    }
 
 }
 
