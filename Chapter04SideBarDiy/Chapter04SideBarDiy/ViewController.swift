060be8af36d599f1e68dec465945e151f055dab4
leeds1204 <leeds1204@gaeasoft.co.kr>
2019-01-28 14:45:55 +0900

    [ Chapter04 ] 셀프 슬라이드 프로젝트 생성

---
+++ Chapter04SideBarDiy/Chapter04SideBarDiy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chapter04SideBarDiy
+//
+//  Created by GAEA on 2019. 1. 28..
+//  Copyright © 2019년 gaeasoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
