98793fb8e0f93c166d6894625efa9a8e3ad1d0c0
leeds1204 <leeds1204@gaeasoft.co.kr>
2019-01-28 18:59:55 +0900

    [ Chapter04 ] 화면 제스처 추가 및 제스처별 메서드 분기처리

--- Chapter04SideBarDiy/Chapter04SideBarDiy/FrontViewController.swift
+++ Chapter04SideBarDiy/Chapter04SideBarDiy/FrontViewController.swift
@@ -12,17 +12,35 @@ class FrontViewController: UIViewController {
         
         self.navigationItem.leftBarButtonItem = btnSideBar
         
+        
+        // 화면 제스처 이벤트 추가 (왼쪽)
+        let dragLeft = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(moveSide(_:)))
+        dragLeft.edges = UIRectEdge.left
+        self.view.addGestureRecognizer(dragLeft)
+        
+        // 화면 제스처 이벤트 추가 (오른쪽)
+        let dragRight = UISwipeGestureRecognizer(target: self, action: #selector(moveSide(_:)))
+        dragRight.direction = .left
+        self.view.addGestureRecognizer(dragRight)
+        
     }
     
     
     // 사용자 액션에 따라 딜리게이트 메소드 추가
     @objc func moveSide(_ sender: Any){
-     
-        if self.delegate?.isSideBarShowing == false {
+        
+        if sender is UIScreenEdgePanGestureRecognizer {
+            // 왼쪽에서 우측으로 화면 이벤트
             self.delegate?.openSideBar(nil)
-            
-        }else{
+        }else if sender is UISwipeGestureRecognizer {
+            // 중앙에서 좌측으로 화면 이벤트
             self.delegate?.closeSideBar(nil)
+        }else if sender is UIBarButtonItem {
+            if self.delegate?.isSideBarShowing == false {
+                self.delegate?.openSideBar(nil)
+            }else{
+                self.delegate?.closeSideBar(nil)
+            }
         }
     }
 
