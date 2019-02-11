d2e4e3c0d63ab11d374b175b60365a525cb15632
leeds1204 <leeds1204@gaeasoft.co.kr>
2019-02-11 09:26:12 +0900

    [ Chapter05 ] UITapGestureRecognizer 추가 (이름필드)

--- Chapter05UserDefault/Chapter05UserDefault/ListTableViewController.swift
+++ Chapter05UserDefault/Chapter05UserDefault/ListTableViewController.swift
@@ -7,6 +7,12 @@ class ListTableViewController: UITableViewController {
     @IBOutlet weak var gender: UISegmentedControl!
     @IBOutlet weak var married: UISwitch!
     
+    // 이름 필드 선택시
+    @IBAction func edit(_ sender: UITapGestureRecognizer) {
+        // 이름 수정
+        self.nameEdit()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -53,32 +59,37 @@ class ListTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         if indexPath.row == 0 { // 첫번쨰 셀이 클릭 되었을 때에만
-            let alert = UIAlertController(title: nil, message: "이름을 입력하세요", preferredStyle: UIAlertController.Style.alert)
-            
-            // 입력 필드 추가
-            alert.addTextField { (textfield : UITextField) in
-                textfield.text = self.name.text // name 레이블의 텍스트를 입력폼에 기본값으로 넣어준다.
-            }
-            
-            // 버튼 및 액션 추가
-            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (UIAlertAction) in
-                // 사용자가 OK 버튼을 누르면 입력 필드에 입력된 값을 저장한다.
-                let value = alert.textFields?[0].text   // alert View 텍스트 필드
-                let plist = UserDefaults.standard   // 기본저장소 가져온다.
-                plist.set(value, forKey: "name") // name키값으로 이름 저장
-                plist.synchronize() // 동기화
-                self.name.text = value
-                
-                // LOG
-                let valueString = plist.string(forKey: "name")
-                print("UserDefaults Data : name = '\(String(describing: valueString))'")
-            }))
-            
-            // 알림창을 띄운다.
-            self.present(alert, animated: false, completion: nil)
+            self.nameEdit() // 이름 수정
         }
     }
     
+    
+    // 이름 필드 입력
+    func nameEdit(){
+        let alert = UIAlertController(title: nil, message: "이름을 입력하세요", preferredStyle: UIAlertController.Style.alert)
+        
+        // 입력 필드 추가
+        alert.addTextField { (textfield : UITextField) in
+            textfield.text = self.name.text // name 레이블의 텍스트를 입력폼에 기본값으로 넣어준다.
+        }
+        
+        // 버튼 및 액션 추가
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (UIAlertAction) in
+            // 사용자가 OK 버튼을 누르면 입력 필드에 입력된 값을 저장한다.
+            let value = alert.textFields?[0].text   // alert View 텍스트 필드
+            let plist = UserDefaults.standard   // 기본저장소 가져온다.
+            plist.set(value, forKey: "name") // name키값으로 이름 저장
+            plist.synchronize() // 동기화
+            self.name.text = value
+            
+            // LOG
+            let valueString = plist.string(forKey: "name")
+            print("UserDefaults Data : name = '\(String(describing: valueString))'")
+        }))
+        
+        // 알림창을 띄운다.
+        self.present(alert, animated: false, completion: nil)
+    }
     
     // MARK: 스토리보드의 화면 객체를 사용(Static) 상위 테이블뷰 클래스의 메서드 오버라이드 사용
     // MARK: - Table view data source
