e9790ca98b80eca5ad00362449c7b250b651ac21
leeds1204 <leeds1204@gaeasoft.co.kr>
2019-03-20 18:40:12 +0900

    [ Chapter07 - CoreData ] 프로젝트 생성

---
+++ Chapter07_CoreData/Chapter07_CoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chapter07_CoreData
+//
+//  Created by GAEA on 2019. 3. 20..
+//  Copyright © 2019년 gaeasoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
