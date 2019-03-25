6d3025e16dad2acdc9d4f060a1f769825362a171
leeds1204 <leeds1204@gaeasoft.co.kr>
2019-03-25 13:07:19 +0900

    [Charpter08-APITEST] JSON 요청 방식 추가

--- Chapter08_APITest/Chapter08_ApiTest/Chapter08_ApiTest/ViewController.swift
+++ Chapter08_APITest/Chapter08_ApiTest/Chapter08_ApiTest/ViewController.swift
@@ -24,8 +24,12 @@ class ViewController: UIViewController {
     }
     
     @IBAction func post(_ sender: Any) {
-        self.requestPostEchoAPI()
+        self.requestPostXMLEchoAPI()
     }
+    @IBAction func json(_ sender: Any) {
+        self.requestPostJsonEchoAPI()
+    }
+    
     
     // MARK: - Public
     func setText(time: String) {
@@ -33,22 +37,92 @@ class ViewController: UIViewController {
 //        self.currentTime.sizeToFit()
     }
     
+    func getUserInfo() -> [String] {
+        // 전송할 값 준비
+        let userID = (self.userID.text)!
+        let userName = (self.name.text)!
+        
+        return [userID,userName]
+    }
     
     
     // MARK: - Request
-    func requestPostEchoAPI(){
+    func requestPostJsonEchoAPI(){
+        let userInfo = self.getUserInfo()
+        let userID = userInfo[0]
+        let userName = userInfo[1]
         
-        // 전송할 값 준비
-        let userID = (self.userID.text)!
-        let name = (self.name.text)!
-        let param = "userID=\(userID)&name=\(name)"
+        let param = ["userID": userID, "name":userName]
+        let paramData = try! JSONSerialization.data(withJSONObject: param, options: [])
+        
+        let url = URL(string: "http://swiftapi.rubypaper.co.kr:2029/practice/echoJSON")
+        
+        var request = URLRequest(url: url!)
+        request.httpMethod = "POST"
+        request.httpBody = paramData
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue(String(paramData.count), forHTTPHeaderField: "Content-Length")
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            // 응답 실패
+            if let error = error {
+                NSLog("An error ha occurred \(error.localizedDescription)")
+                return
+            }
+            
+            NSLog("Response Data = \(String(data: data!, encoding: String.Encoding.utf8)!)")
+            
+            // 응답 성공
+            DispatchQueue.main.async {
+                do {
+                    
+                    let responseDic = try JSONSerialization.jsonObject(with: data!, options: [] ) as? NSDictionary
+                    
+                    guard let jsonObject = responseDic else { return }
+                    
+                    let result = jsonObject["result"] as? String
+                    let timestamp = jsonObject["timestamp"] as? String
+                    let userID = jsonObject["userID"] as? String
+                    let name = jsonObject["name"] as? String
+                    
+                    if result == "SUCCESS" {
+                        self.responseView.text =
+                            "아이디 : \(userID ?? "")" + "\n"
+                            + "이름 : \(name ?? "")" + "\n"
+                            + "응답결과 : \(result ?? "")" + "\n"
+                            + "응답시간 : \(timestamp ?? "")" + "\n"
+                            + "요청방식 : JSON"
+                    }
+                    
+                    
+                } catch let e as NSError {
+                    NSLog("An Error ha occurred while parsing JSONObject \(e.localizedDescription)")
+                }
+                
+                
+            }
+            
+            
+        }
+        
+        task.resume()
+    }
+    
+
+    func requestPostXMLEchoAPI(){
+
+        let userInfo = self.getUserInfo()
+        let userID = userInfo[0]
+        let userName = userInfo[1]
+        
+        let param = "userID=\(userID)&name=\(userName)"
         
         // 인코딩
         let paramData = param.data(using: String.Encoding.utf8)
         
         // URL 객체 정의
         let url = URL(string: "http://swiftapi.rubypaper.co.kr:2029/practice/echo")
-        // let url = URL(string: "http://swiftapi.rubypaper.co.kr:2029/practice/echoJSON")
         
         // URL Request 객체 정의
         var request = URLRequest(url: url!)
